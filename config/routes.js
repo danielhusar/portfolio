343a1341837b4fde4db2b3ed0137f82e0c9194ea
Daniel <dano.husar@gmail.com>
2013-03-25 16:25:03 +0100

    logic changes, server grunt remove

--- config/routes.js
+++ config/routes.js
@@ -2,8 +2,28 @@
 	*  ALL THE STATIC ROUTES WITH ANONYMOUS FUNCTIONS
 	*  THIS ROUTES HERE OVERRIDES THE ROUTES DEFINED IN CONTROLLERS
 **/
-module.exports = function(app, helpers){
-	app.get('/test', function(req, res){
-		helpers.template(res, 'index.html', {});
+module.exports = function(app){
+
+	var helpers = require('../app/helpers/helpers');
+	/**
+
+  EXAMPLE RULE:
+
+		app.get('/test', function(req, res){
+			helpers.template(res, 'index.html');
+		});
+
+		app.get('/404', function(req, res){
+		  res.redirect('/');
+		});
+
+	**/
+
+	//redirect all errors to homepage, this must be the last rule
+	app.use(function(req,res){
+		res.redirect('/');
 	});
+
+
+	return app;
 };
