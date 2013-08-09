43bbff854efd785d557598b2403fba0d9dd716c0
Daniel <dano.husar@gmail.com>
2013-08-10 00:49:14 +0200

    test

--- app/controllers/portfolio.js
+++ app/controllers/portfolio.js
@@ -19,7 +19,7 @@ module.exports = function(app, model, helpers){
 	});
 
 	/**
-	 * Ajax loader for the photos
+	 * Ajax loader for the photoss
 	 */
 	app.get('/ajax/photos/:page', function(req, res){
 		var page = req.params.page || 1;
